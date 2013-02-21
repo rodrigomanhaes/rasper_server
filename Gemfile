e552ddf1c3aac2e5ce223c7162cd49ee0b72b872
Rodrigo Manhães <rmanhaes@gmail.com>
2013-02-20 21:10:37 -0300

    open house

---
+++ Gemfile
@@ -0,0 +1,16 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.12'
+
+gem 'activerecord-jdbcsqlite3-adapter'
+
+gem 'jruby-openssl'
+
+group :assets do
+  gem 'sass-rails', '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+  gem 'therubyrhino'
+  gem 'uglifier', '>= 1.0.3'
+end
+
+gem 'jquery-rails'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RasperServer::Application.config.secret_token = '5c90d1e444c659ac2784a0664653f7599591c109f786e32f8d7b34a3ae1c2a096880ab9a0c4634c28e768abdb121f417a312e7c997deecdca66c019ad84a2b15'
