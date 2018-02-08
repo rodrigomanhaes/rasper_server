870d3265b1b788aa9e90bbaeff0524273aeaaa9d
Rodrigo Manhães <rmanhaes@gmail.com>
2018-02-08 11:45:00 -0200

    small refactoring

--- spec/support/dirs.rb
+++ spec/support/dirs.rb
@@ -1,11 +1,17 @@
-def resources_dir
-  File.expand_path(File.join(File.dirname(__FILE__), '..', 'resources'))
+module DirUtils
+  def resources_dir
+    File.expand_path(File.join(File.dirname(__FILE__), '..', 'resources'))
+  end
+
+  def resource(name)
+    File.read(File.join(resources_dir, name))
+  end
+
+  def report_dir
+    File.expand_path(File.join(File.dirname(__FILE__), '..', '..', 'report'))
+  end
 end
 
-def resource(name)
-  File.read(File.join(resources_dir, name))
-end
-
-def report_dir
-  File.expand_path(File.join(File.dirname(__FILE__), '..', '..', 'report'))
+RSpec.configure do |config|
+  config.include DirUtils
 end
